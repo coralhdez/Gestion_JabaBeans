000fcde5414c8491a6c90dc7124c0540a82c6b15
Marcosss2 <marcosparra20@hotmail.com>
2022-01-17 11:32:12 +0100

    .
    .

---
+++ Gestion_JavaBeans/src/main/java/Beans/Beans.java
@@ -0,0 +1,42 @@
+
+package Beans;
+
+import java.io.Serializable;
+
+
+public class Beans implements Serializable {
+   private int base;
+   private int altura;
+
+   
+    public Beans() {
+    }
+
+    public Beans(int base, int altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+   
+    
+    public int getBase() {
+        return base;
+    }
+
+    public void setBase(int base) {
+        this.base = base;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+  
+   public int getArea(int base, int altura){
+       
+       return base * altura;
+   }
+}
